3413a8afe1edab9944dacbf3e557f002d4e1141f
Draghtnim <166871112+Draghtnim@users.noreply.github.com>
2025-12-25 17:24:24 +0100

    test3

--- obj/Debug/netstandard2.1/DarkFountains.AssemblyInfo.cs
+++ obj/Debug/netstandard2.1/DarkFountains.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DarkFountains")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9a62729077e8ae2f5356e57a1c0311d97ddae454")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6b3ae25b5ecdddae15ff9242c78b20605a2a9758")]
 [assembly: System.Reflection.AssemblyProductAttribute("DarkFountains")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DarkFountains")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
